f1bc5e3766618de218875a19ac29beb9101a3809
devyosse <narutoshippuden884@gmail.com>
2021-12-07 11:52:42 +0100

    pendu

--- app.js
+++ app.js
@@ -1,31 +1,52 @@
-
-let pVerte = document.getElementById('pverte');
-let pBleue = document.getElementById('pbleue');
-let pMarron = document.getElementById('pmarron');
-
-let dechet = [
-    burger = ["./imgtri/burger.JPG"],
-    carton = ["./imgtri/carton.JPG"],
-    papier = ["./imgtri/papier.JPG"],
-    papiert = ["./imgtri/papiertoilet.JPG"],
-    parfum = ["./imgtri/parfum.JPG"],
-    pizza = ["./imgtri/pizza.JPG"],
-    sac = ["./imgtri/sac.JPG"],
-    vinr = ["./imgtri/vin.JPG"],
+const dechets = [
+    "burger",
+    "papier",
+    "papiertoilet",
+    "parfum",
+    "pizza",
+    "vin",
 ];
 
-let tab1 = [];
-let tab2 = [];
-let tab3 = [];
-let tab4 = [];
-
-let strt = document.getElementById('start');
-let image = document.createElement('img');
-let img = document.getElementById('image');
+let dechetsJaune = ["carton","sac"];
+let dechetsBleue = [""];
 
 
-strt.addEventListener('click', function (){
-    let randomword = Math.floor(Math.random() * dechet.length);
-    img.appendChild(image);
-    image.src = dechet[randomword];
+let random = Math.floor(Math.random() * dechets.length);
+let img = document.getElementById('imgRandom');
+let poubelleJaune = document.getElementById('pjaune');
+let poubelleBleue = document.getElementById('pbleue');
+let poubelleMarron = document.getElementById('pmarron');
+let poubelleVerte = document.getElementById('pverte');
+let point = 0;
+
+function dechet(){
+    random = Math.floor(Math.random() * dechets.length);
+    img.src = "/imgtri/" +dechets[random] + ".JPG";
+}
+
+//image aléatoire
+document.getElementById('start').addEventListener('click', function (){
+    dechet();
 });
+
+poubelleJaune.addEventListener('click', function (){
+    dechet();
+});
+
+poubelleBleue.addEventListener('click', function (){
+    dechet();
+});
+
+poubelleMarron.addEventListener('click', function (){
+    dechet();
+});
+
+poubelleVerte.addEventListener('click', function (){
+    dechet();
+});
+
+
+
+function VerifDechets(){
+
+}
