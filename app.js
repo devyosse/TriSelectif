57675820596f0e2a80e70d6b9e6d34d80d95770f
devyosse <narutoshippuden884@gmail.com>
2021-12-07 12:41:02 +0100

    pendu

--- app.js
+++ app.js
@@ -1,52 +1,63 @@
-const dechets = [
-    "burger",
-    "papier",
-    "papiertoilet",
-    "parfum",
-    "pizza",
-    "vin",
-];
+let dechetsJaune = ["carton","sac", "papiertoilet"];
+let dechetsBleue = ["journal", "papier"];
+let dechetsMarron = ["pizza", "burger"];
+let dechetsVerte = ["parfum", "vin"];
 
-let dechetsJaune = ["carton","sac"];
-let dechetsBleue = [""];
-
-
-let random = Math.floor(Math.random() * dechets.length);
 let img = document.getElementById('imgRandom');
 let poubelleJaune = document.getElementById('pjaune');
 let poubelleBleue = document.getElementById('pbleue');
 let poubelleMarron = document.getElementById('pmarron');
 let poubelleVerte = document.getElementById('pverte');
 let point = 0;
+let score = document.getElementById('score');
+let texte = document.getElementById('text');
 
-function dechet(){
-    random = Math.floor(Math.random() * dechets.length);
-    img.src = "/imgtri/" +dechets[random] + ".JPG";
+function dechet() {
+    let dechets = dechetsJaune.concat(dechetsBleue, dechetsMarron, dechetsVerte);
+    let random = Math.floor(Math.random() * dechets.length);
+    img.src = "/imgtri/" + dechets[random] + ".JPG";
+    img.title = dechets[random];
 }
 
-//image aléatoire
+function verifDechets(dechets){
+    if(dechets.includes(img.title)){
+        point++;
+        let index = dechets.indexOf(img.title);
+        dechets.splice(index, 1);
+        texte.innerHTML = "Bonne réponse !";
+    }
+    else{
+        point--;
+        texte.innerHTML = "Faux ce n'est pas la bonne poubelle";
+    }
+    score.innerHTML = point;
+}
+
+
 document.getElementById('start').addEventListener('click', function (){
     dechet();
 });
 
-poubelleJaune.addEventListener('click', function (){
+poubelleJaune.addEventListener('click', function () {
+    verifDechets(dechetsJaune);
     dechet();
 });
 
 poubelleBleue.addEventListener('click', function (){
+    verifDechets(dechetsBleue);
     dechet();
 });
 
 poubelleMarron.addEventListener('click', function (){
+    verifDechets(dechetsMarron);
     dechet();
 });
 
 poubelleVerte.addEventListener('click', function (){
+    verifDechets(dechetsVerte);
     dechet();
 });
 
 
 
-function VerifDechets(){
 
-}
